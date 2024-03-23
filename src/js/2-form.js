29281bb3076f259e15f0fc3523072905ac8be0a1
HelenYakovenko <elenus.light@gmail.com>
2024-03-23 22:06:33 +0200

    js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,2 +1,44 @@
+
 const form = document.querySelector(".feedback-form");
+const FORM_KEY = "feedback-form-state";
+
+form.addEventListener("input", saveFormData);
+form.addEventListener("submit", submitForm);
+
+
+
+function saveFormData() {
+   const dataForm = {
+      email: form.email.value.trim(),
+      message: form.message.value.trim(),
+   }
+
+   localStorage.setItem(FORM_KEY, JSON.stringify(dataForm));
+   // console.log(dataForm);
+}  
+
+const checkForm = localStorage.getItem(FORM_KEY)
+if (checkForm) {
+   const parsedData = JSON.parse(checkForm);
+   form.email.value = parsedData.email || "";
+   form.message.value = parsedData.message || "";
+}
+
+
+function submitForm(event) {
+   event.preventDefault();
+   const sendDataForm = {
+      email: form.email.value.trim(),
+      message: form.message.value.trim(),
+   }
+   if (sendDataForm.email === "" || sendDataForm.message === "") {
+      return alert("Both fields must be filled before sending")
+   }
+   console.log(sendDataForm);
+
+   localStorage.removeItem(FORM_KEY);
+   form.reset()
+}
+
+
 
